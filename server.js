6b54ad90dd2b75db29d5f9d4bc1e25f3c07d1d17
chinnikrishwanth <chinnikrishwanth@gmail.com>
2025-10-31 16:08:37 +0530

    Initial commit

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ app.use('/api/auth', authRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
